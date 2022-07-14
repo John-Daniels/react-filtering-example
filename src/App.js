7aac978d0fe7636162c12f0828c846bedcfd4042
John Daniels <adeyemijohndaniels@gmial.com>
2022-07-14 05:10:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import "./App.scss"
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>Welcome to the app...</h1>
+    </div>
+  )
+}
+
+export default App
